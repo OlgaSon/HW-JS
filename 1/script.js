918d3b68930d6552a880a3ffa0861c90a74fa0b6
Olga Petelskaya <o.petelskaya@gmail.com>
2021-12-08 16:43:53 +0300

    Add first JS homework

---
+++ 1/script.js
@@ -0,0 +1,11 @@
+function result (object) {
+let a = 2;
+let b = "apple";
+let c = a;
+a = b;
+b = c;
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = arr[0] + arr[9] + arr[4];
+object = {a, b, sum};
+console.log (result);
+};
---
+++ 1/script1.js
@@ -0,0 +1,7 @@
+let a = 2;
+let b = "apple";
+console.log (a, b)
+let c = a;
+a = b;
+b = c;
+console.log (a, b)
