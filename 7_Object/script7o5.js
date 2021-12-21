6b0de379295ea269981f1a2c07c2691fb7db6f99
Olga Petelskaya <o.petelskaya@gmail.com>
2021-12-21 23:54:57 +0300

    Add 7: task 5

---
+++ 7_Object/script7o5.js
@@ -0,0 +1,14 @@
+const rectangle = {
+    height: 3,
+    width: 2,
+    getArea() {
+        return rectangle.height * rectangle['width']
+    },
+};
+
+console.log(rectangle.getArea())
+
+
+/* Задание 5.
+Создать объект rectangle с параметрами height и width и 
+методом getArea(), который возвращает площадь прямоугольника.*/
