4e4d5ccda7859ef66a1d071557eca7c59238b950
momo-levin <86719961+momo-levin@users.noreply.github.com>
2021-12-21 17:23:42 +0300

    Update script2.js

--- 2_challenge/script2.js
+++ 2_challenge/script2.js
@@ -1,7 +1,7 @@
 let arr = [];
 let i = 0;
 b = 0;
-while (b < 999999999) {
+while (b < 999999999) {//i,b никак не пояснены. "нипаняиный" код
     function fibi(i) {
         if (i === 0 || i === 1) {
             return arr[i] = i;
