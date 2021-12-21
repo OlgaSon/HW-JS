fb5f002e48646582d5991fffd4a137f69bf4753d
Olga Petelskaya <o.petelskaya@gmail.com>
2021-12-22 02:34:45 +0300

    Add 7: task 4

---
+++ 7_ObjectAdd/script7oa4.js
@@ -0,0 +1,11 @@
+let product = {
+    name: 'John',
+    price: 30,
+    sold: false,
+};
+
+let [const1, const2, const3] = Object.keys(product);
+console.log(const1, const2, const3)
+
+/* Задача 4
+Есть объект. Присвоить каждое его свойство к отдельной переменной.*/
