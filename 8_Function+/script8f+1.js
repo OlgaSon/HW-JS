86ff3303afac91160177ba9fc6eeccaa0593d63b
Olga Petelskaya <o.petelskaya@gmail.com>
2021-12-28 15:23:35 +0300

    Add 8: task 1

---
+++ 8_Function+/script8f+1.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const printMessage = (n) => {
+    setInterval(() => alert("message"), n);
+};
+printMessage(2000);
+
+
+/* Задача 1.
+Напишите функцию printMessage(), которая выводит сообщения на экран каждые n секунд.*/
